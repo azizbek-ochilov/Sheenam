a93309df2de5cc22b6f6c5e97566832a5d80dbc9
Azizbek Ochilov <99602759+azizbek-ochilov@users.noreply.github.com>
2023-02-12 11:51:39 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.Api.Test.Unit/DeleteMe.cs
@@ -0,0 +1,18 @@
+//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//=================================
+
+using Xunit;
+
+namespace Sheenam.Api.Test.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
