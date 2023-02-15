35123f9dee5041a8200794039fdbe4e26b2cb0a7
Azizbek Ochilov <99602759+azizbek-ochilov@users.noreply.github.com>
2023-02-15 20:01:48 +0500

    ShouldThrowValidationExeptionOnAddIfGuestNullAndLogItAsync -> WIP

--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -3,8 +3,10 @@
 // Free To Use To Find Comfort and Peace
 //=================================
 
+using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Guests;
+using System;
 using System.Threading.Tasks;
 
 namespace Sheenam.Api.Services.Foundations.Guests
@@ -12,11 +14,20 @@ namespace Sheenam.Api.Services.Foundations.Guests
     public class GuestService : IGuestService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public GuestService(IStorageBroker storageBroker) =>
-            this.storageBroker = storageBroker;
+        public GuestService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+            {
+                this.storageBroker = storageBroker;
+                this.loggingBroker = loggingBroker;
+            }
 
-        public async ValueTask<Guest> AddGuestAsync(Guest guest) =>
-            await this.storageBroker.InsertGuestAsync(guest);
+        public async ValueTask<Guest> AddGuestAsync(Guest guest)
+        {
+            this.loggingBroker.LogError(new Exception("Something"));
+            return await this.storageBroker.InsertGuestAsync(guest);
+        }
     }
 }
